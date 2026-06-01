38be8563ddb0446e1834f05c2ce4406c08dbd59b
fella <boudoumifella@email.com>
2026-06-01 08:56:55 +0100

    fix: add admin user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,25 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        // Crée l'admin
+        User::firstOrCreate(
+            ['email' => 'boudoumifella@gmail.com'],
+            [
+                'name'     => 'Admin',
+                'email'    => 'boudoumifella@gmail.com',
+                'password' => Hash::make('password'),
+                'is_admin' => true,
+            ]
+        );
+
+        // Crée les packs d'hébergement
         $this->call([
             HostingSeeder::class,
         ]);
